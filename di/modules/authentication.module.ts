cbe30e8f57186209dbc38a3d8da27fee462ccb95
glrd <devglrd@gmail.com>
2024-09-07 17:53:30 +0200

    Fix

--- di/modules/authentication.module.ts
+++ di/modules/authentication.module.ts
@@ -1,7 +1,7 @@
 import {ContainerModule, interfaces} from "inversify";
 import {DI_SYMBOLS} from "@/di/types";
-import {IAuthenticationService} from "@/core/ports/authentication.interface";
 import {AuthenticationService} from "@/core/adapters/auth.gateway";
+import {IAuthenticationService} from "@/core/ports/auth.interface";
 
 const initializeModule = (bind: interfaces.Bind) => {
     bind<IAuthenticationService>(DI_SYMBOLS.IAuthenticationService).to(AuthenticationService);
